6527c6c325e12e713ef650bf6b01645294049a6e
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-09-26 15:36:19 +0000

    add truncate - final

--- main.sql
+++ main.sql
@@ -2,6 +2,6 @@
 Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2356. Truncate your answer to  decimal places.
 */
 
-SELECT sum(lat_n)
+SELECT truncate(sum(lat_n),4)
 FROM station
 WHERE lat_n BETWEEN 38.7880 AND 137.2356
