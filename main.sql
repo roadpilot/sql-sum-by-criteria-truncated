a5a0126c76df5656fc57f3736a7a99f006a97c87
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-09-26 15:35:48 +0000

    test between

--- main.sql
+++ main.sql
@@ -1,3 +1,7 @@
 /*
 Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2356. Truncate your answer to  decimal places.
 */
+
+SELECT lat_n
+FROM station
+WHERE lat_n BETWEEN 38.7880 AND 137.2356
